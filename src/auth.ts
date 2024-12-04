69c1dbf02f2022409d76cccb467c12ba80e10fbc
Muhammad Hamid <61172566+hamidkhans651@users.noreply.github.com>
2024-12-04 15:08:49 +0500

    .

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,11 @@
 import NextAuth, { CredentialsSignin } from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import CredentialsProvider from "next-auth/providers/credentials"
-import Email from "next-auth/providers/email" 
+import Email from "next-auth/providers/email"
 import GoogelProvider from "next-auth/providers/google"
 import { users } from "./schema"
+import { compare } from 'bcryptjs'
+import { User } from "./models/user"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
@@ -23,14 +25,25 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                 const Email = Credentials.Email as string | undefined
                 const password = Credentials.password as string | undefined
 
-                if (typeof Email !== "string" || !password) 
+                if (typeof Email !== "string" || !password)
                     throw new CredentialsSignin({ cause: "Email and password is not valid" })
 
-                const user = await users.findOne({Email,}).select;
-               
-                if (password !== "passcode")
-                    throw new CredentialsSignin({ cause: "password id not valid " });
-                else return user;
+                const user = await User.findOne({ Email }).select("+password");
+                
+                if (!user)
+                    throw new CredentialsSignin("invalid emial or password");
+
+                if (!user.password)
+                    throw new CredentialsSignin("invalid emial or password");
+
+                const isMatch = await compare(password, user.password)
+
+                if (!isMatch)
+                    throw new CredentialsSignin("invalid emial or password");
+
+                
+
+            return {name:user.name, email:user.email, id:user.id};
             }
 
 
